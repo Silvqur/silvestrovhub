24998bcfcb3b1cd4afc0ef6fd0bc567a60e49a06
Silver <silvqur@duck.com>
2025-06-13 14:48:05 +0000

    redesign

--- app/home/page.js
+++ app/home/page.js
@@ -12,65 +12,20 @@ export default function Home() {
   const pages = [{ href: "/home", name: "Home page" }, { href: "/news", name: "News page" }]
 
   return (
-    <main className="bg-[#1f313d]">
+    <main className="w-[70rem] mx-auto">
       <TopBar></TopBar>
-      <div className="relative">
-        <Image
-          src="/third1.png"
-          height={5000}
-          width={5000}
-          alt="???"
-          className="shadow-2xl lg:block hidden"
-        />
-        <Image
-          src="/5.png"
-          height={5000}
-          width={5000}
-          alt="???"
-          className="shadow-2xl lg:hidden block"
-        />
-        <p className="absolute top-[330px] right-[650px] text-6xl font-extrabold lg:block hidden">THE FUTURE<span className="underline mx-2">BEST IT</span>COMPANY IN<span className="ml-2 underline">RUSSIA</span></p>
-        <p className="absolute top-[100px] text-center text-white text-2xl font-extrabold lg:hidden block">THE FUTURE<span className="underline mx-2">BEST IT</span>COMPANY IN<span className="ml-2 underline">RUSSIA</span></p>
-      </div>
-      <Link href="#" className="flex justify-center mx-auto mt-3">
-        <div className="w-full bg-[#345166] p-4">
-          <p className="text-[#e4c8b2] lg:text-3xl hover:underline flex justify-center text-xl font-medium">You can <span className="font-bold hover:underline mx-3 shadow-sm">apply for a job</span>on our website.</p>
-        </div>
-      </Link>
-      <div className="bg-[#345166] p-4 mt-6 lg:mt-10">
-        <p className="text-white text-xl font-extrabold lg:text-2xl">Announcements about the company:</p>
-        <div className="flex overflow-x-auto lg:flex-wrap my-7 gap-7">
-          {data.map((itm, index) => (
-            <div className="flex" key={index}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="lg:size-11 size-8 flex-none mr-2 text-[#25383d] border-[1px] border-[#273a40] rounded-lg shadow-lg">
-                <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-              </svg>
-              <div className="min-w-0">
-                <h1 className="text-[#c4c4bc] text-xl font-bold text-nowrap">{itm.title}</h1>
-                <p className="text-[#a3a39b] max-w-[15rem] break-words">{itm.desc}</p>
-                <p className="text-[#a3a39b] mt-2">{itm.date}</p>
+      <div className="flex">
+        <div className="w-full"></div>
+        <div className="w-[30rem] border-l-[1px] border-gray-200 pt-5 px-2">
+          <div className="space-y-8">
+            {data.map((itm, index) => (
+              <div key={index} className="">
+                <h1 className="text-black font-bold text-xl text-center">{itm.title}</h1>
+                <p className="text-black mt-2">{itm.desc}</p>
+                <p className="text-blue-500 text-sm/10">{itm.date}</p>
               </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="bg-[#345166] p-2 lg:p-4 mt-6 lg:mt-10">
-        <div>
-          <Link href="/socialnetwork">
-            <h1 className="text-[#935739] text-center font-extrabold text-3xl underline underline-offset-4">Forward to SocialNetwork</h1>
-          </Link>
-        </div>
-      </div>
-      <div className="bg-[#345166] p-2 lg:p-4 mt-6 lg:mt-10">
-        <p className="text-white font-extrabold lg:text-2xl">All avaible pages:</p>
-        <div className="flex  mt-5">
-          {pages.map((itm, index) => (
-            <div key={index}>
-              <Link href={itm.href} className="flex ml-7">
-                <p className="text-xl text-[#a3a39b] hover:text-blue-600 font-bold hover:underline delay-100 flex p-3">{itm.name}</p>
-              </Link>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </main>
--- components/topheader.js
+++ components/topheader.js
@@ -3,49 +3,31 @@ import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 
 export default function TopBar() {
 
-    const solutions = [
-        { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: "NewsPaper" },
-        { name: 'Engagement', description: 'Speak directly to your customers', href: '#', },
-        { name: 'Security', description: "Your customers' data will be safe and secure", href: '#', },
-        { name: 'Integrations', description: 'Connect with third-party tools', href: '#', },
-        { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', },
+    const data = [
+        { name: "SocialNetwork", href: "/socialnetwork" },
+        { name: "News", href: "/news" }
     ]
 
     return (
-        <div className="bg-[#273a40] flex justify-between p-3 sticky top-0 px-2 lg:px-5 z-50">
+        <div className="bg-white flex justify-between p-3 sticky top-0 px-2 lg:px-5 z-50 border-b-[1px] border-gray-200">
             <Link href="/home" className="">
                 <div className="flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="198.943 203.345 83.627 80.986" className=" size-7 lg:size-12 border-[1px] border-[#935739] mr-1 lg:mr-2">
-                        <text style={{ whiteSpace: 'pre', fill: 'rgb(147, 87, 57)', fontFamily: 'Arial, sans-serif', fontSize: '28px' }} x="218.31" y="238.557">SV</text>
-                        <text style={{ whiteSpace: 'pre', fill: 'rgb(147, 87, 57)', fontFamily: 'Arial, sans-serif', fontSize: '28px' }} x="211.268" y="265.845">HUB</text>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="198.943 203.345 83.627 80.986" className=" size-7 lg:size-12 text-blue-500 mr-1 lg:mr-2">
+                        <text style={{ whiteSpace: 'pre', fill: 'rgb(59, 130, 246)', fontFamily: 'Arial, sans-serif', fontSize: '28px' }} x="218.31" y="238.557">SV</text>
+                        <text style={{ whiteSpace: 'pre', fill: 'rgb(59, 130, 246)', fontFamily: 'Arial, sans-serif', fontSize: '28px' }} x="211.268" y="265.845">HUB</text>
                     </svg>
-                    <p className="text-[#935739] text-xl lg:text-4xl self-center">SILVESTROV HUB</p>
+                    <p className="text-black text-xl lg:text-4xl self-center">SILVESTROV HUB</p>
                 </div>
             </Link>
-            <Popover className="relative">
-                <PopoverButton className="">
-                    <p className="text-[#935739] lg:text-3xl self-center text-xl">Menu</p>
-                </PopoverButton>
-                <PopoverPanel className="absolute right-0 z-50" transition>
-                    <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-[#545748] text-sm/6 shadow-lg">
-                        <div className="p-4">
-                            {solutions.map((item) => (
-                                <div key={item.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-[#2f3a32]">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#3e2412]">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                    </svg>
-                                    <div>
-                                        <a href={item.href} className="font-semibold text-gray-900">
-                                            {item.name}
-                                        </a>
-                                        <p className="mt-1 text-gray-600">{item.description}</p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+            <div className="flex gap-x-6">
+                {data.map((itm, index) => (
+                    <div key={index} className="self-center">
+                        <Link href={itm.href} className="hover:underline hover:underline-offset-4 text-blue-500 delay-100">
+                            <p className="text-black text-xl self-center">{itm.name}</p>
+                        </Link>
                     </div>
-                </PopoverPanel>
-            </Popover>
+                ))}
+            </div>
         </div>
     )
 }
