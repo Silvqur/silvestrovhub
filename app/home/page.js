17be0f24caeb70c2ca1ad3513caabfab4a923060
Silver <silvqur@duck.com>
2025-06-13 12:25:39 +0000

    fix

--- app/home/page.js
+++ app/home/page.js
@@ -6,7 +6,7 @@ export default function Home() {
 
   const data = [
     { title: "Site is online!", desc: "Congratulations for my team) ", date: "Jun 6 12:34 PM" },
-    { title: "SocialNetwork", desc: "We strted a social network on our website. Link below", date: "Jun 12 10:11 PM" },
+    { title: "SocialNetwork", desc: "We started a social network on our website. Link below", date: "Jun 12 10:11 PM" },
     { title: "The day of no terurn", desc: "The Google SEO just indexed our website. Now when you search the name 'silvestrov hub' we are on the first place!", date: "Jun 13 03:10 PM" },
   ]
   const pages = [{ href: "/home", name: "Home page" }, { href: "/news", name: "News page" }]
