d2edc3ba1be03805823ab21fa9958944e5363e2c
Silver <silvqur@duck.com>
2025-07-01 21:44:49 +0300

    Hot fix

--- app/about/page.js
+++ app/about/page.js
@@ -82,7 +82,7 @@ export default function About() {
                     <div className="pt-7 pb-10">
                         <div className="pt-7 border-t-[1px] border-[#1726506c] flex gap-x-3">
                             <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500"><path fillRule="evenodd" d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"></path></svg>
-                            <p>dn606304@gmail.com</p>
+                            <p className="text-white">dn606304@gmail.com</p>
                         </div>
                     </div>
                 </div>
@@ -160,7 +160,7 @@ export default function About() {
                         <div className="pt-7">
                             <div className="pt-7 border-t-[1px] border-[#1726506c] flex gap-x-3">
                                 <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500"><path fillRule="evenodd" d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"></path></svg>
-                                <p>dn606304@gmail.com</p>
+                                <p className="text-white">dn606304@gmail.com</p>
                             </div>
                         </div>
                     </div>
